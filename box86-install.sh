cd66ba99347f8acd8b96b9829a6463df2a2e31ea
GG <jiga90@gmail.com>
2026-06-29 12:56:37 -0700

    Add wget installation to box86-install script

--- box86-install.sh
+++ box86-install.sh
@@ -1,3 +1,4 @@
+sudo apt install wget
 sudo dpkg --add-architecture armhf
 sudo wget https://ryanfortner.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list
 wget -qO- https://ryanfortner.github.io/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg 

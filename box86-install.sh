b31eec9bba65e430dee0c52d1ba9b228e0de59b9
GG <jiga90@gmail.com>
2026-07-10 10:08:22 -0700

    Install libc6 along with box86 for armhf

--- box86-install.sh
+++ box86-install.sh
@@ -2,4 +2,4 @@ sudo apt install wget
 sudo dpkg --add-architecture armhf
 sudo wget https://ryanfortner.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list
 wget -qO- https://ryanfortner.github.io/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg 
-sudo apt update && sudo apt install box86-rpi4arm64:armhf -y
+sudo apt update && sudo apt install box86-rpi4arm64:armhf libc6:armhf -y

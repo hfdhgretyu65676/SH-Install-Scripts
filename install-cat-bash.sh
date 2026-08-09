3821d88aee14a2722c89ab55580bf8266029b061
GG <jiga90@gmail.com>
2026-08-08 19:45:55 -0700

    Add execute permission to Cat_Bash AppImage
    Make the downloaded AppImage executable after download.

--- install-cat-bash.sh
+++ install-cat-bash.sh
@@ -1,4 +1,5 @@
 folder=$(pwd)
 echo "Getting 'Cat_Bash-aarch64.AppImage'..."
 wget https://github.com/hfdhgretyu65676/SH-Install-Scripts/raw/refs/heads/main/Cat_Bash-aarch64.AppImage
+chmod +x $folder/Cat_Bash-aarch64.AppImage
 echo "Done! File is in $folder/Cat_Bash-aarch64.AppImage."

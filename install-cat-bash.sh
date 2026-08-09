1077ea5a72df30a1016a6ee987d9cfa28507b6cf
GG <jiga90@gmail.com>
2026-08-08 19:48:04 -0700

    Add execution instructions for Cat_Bash AppImage
    Updated the installation script to include execution instructions.

--- install-cat-bash.sh
+++ install-cat-bash.sh
@@ -1,5 +1,4 @@
 folder=$(pwd)
 echo "Getting 'Cat_Bash-aarch64.AppImage'..."
 wget https://github.com/hfdhgretyu65676/SH-Install-Scripts/raw/refs/heads/main/Cat_Bash-aarch64.AppImage
-chmod +x $folder/Cat_Bash-aarch64.AppImage
-echo "Done! File is in $folder/Cat_Bash-aarch64.AppImage."
+echo "Done! File is in $folder/Cat_Bash-aarch64.AppImage. Run chmod +x $folder/Cat_Bash-aarch64.AppImage && ./Cat_Bash-aarch64.AppImage to run it."
